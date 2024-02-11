cd6852de3dd7bf5263c239f736cb1b182f1a06b3
fvito <fvito@users.noreply.github.com>
2024-02-11 17:28:56 +0100

    Change wrapper functions to async to match sys crate change
    In JS world, they return promises

--- src/notifications/mod.rs
+++ src/notifications/mod.rs
@@ -8,6 +8,7 @@ mod on_clicked;
 mod on_closed;
 mod on_permission_level_changed;
 
+use std::collections::HashMap;
 pub use web_extensions_sys as sys;
 use crate::Error;
 use crate::util::{js_from_serde, object_from_js, serde_from_js};
@@ -19,22 +20,55 @@ pub use self::{
 };
 
 pub fn notifications() -> sys::Notifications {
-    // Currently we assume a chrome browser and Manifest V3.
+    // Currently we assume a Chrome browser and Manifest V3.
     //
     // Once MV3 is supported by FireFox, we need to check if we can use the same namespace,
     // a shim or our own implementation.
     sys::chrome().notifications()
 }
 
-pub fn clear(notification_id: &str) -> Result<(), Error> {
-    sys::chrome()
+pub async fn clear(notification_id: &str) -> Result<(), Error> {
+    let result = sys::chrome()
         .notifications()
-        .clear(notification_id, None);
+        .clear(notification_id, None)
+        .await;
 
-    Ok(())
+    serde_from_js(result)
 }
 
-pub fn create(
+pub async fn create(
+    notification_id: Option<&str>,
+    options: NotificationOptions<'_>,
+) -> Result<String, Error> {
+    let js_options = js_from_serde(&options)?;
+
+    let result = sys::chrome()
+        .notifications()
+        .create(notification_id, object_from_js(&js_options)?, None)
+        .await;
+
+    serde_from_js(result)
+}
+
+pub async fn get_all() -> Result<HashMap<String, bool>, Error> {
+    let result = sys::chrome()
+        .notifications()
+        .get_all()
+        .await;
+
+    serde_from_js(result)
+}
+
+pub async fn get_permission_level() -> Result<PermissionLevel, Error> {
+    let result = sys::chrome()
+        .notifications()
+        .get_permission_level()
+        .await;
+
+    PermissionLevel::try_from(result)
+}
+
+pub async fn update(
     notification_id: &str,
     options: NotificationOptions<'_>,
 ) -> Result<(), Error> {
@@ -42,30 +76,8 @@ pub fn create(
 
     let result = sys::chrome()
         .notifications()
-        .create(notification_id, object_from_js(&js_options)?, None);
-
-    Ok(())
-}
-
-pub fn get_all() -> Result<(), Error> {
-    sys::chrome()
-        .notifications()
-        .get_all(None);
-
-    Ok(())
-}
-
-pub fn get_permission_level() {}
-
-pub fn update(
-    notification_id: &str,
-    options: NotificationOptions<'_>,
-) -> Result<(), Error> {
-    let js_options = js_from_serde(&options)?;
-
-    let result = sys::chrome()
-        .notifications()
-        .update(notification_id, object_from_js(&js_options)?, None);
+        .update(notification_id, object_from_js(&js_options)?, None)
+        .await;
 
     serde_from_js(result)
 }
